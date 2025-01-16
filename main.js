08ae9c0c8323f64f82e070dd2ecf3911c9e99352
Aditya kumar <adityakr80510@gmail.com>
2025-01-16 10:08:02 +0530

    update

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const axios = require("axios");
-
+const crypto = require("crypto");
+require("dotenv").config();
 const app = express();
 app.use(express.json());
+
+// Helper function to verify the signature
+const verifySignature = (req, secret) => {
+  const signature = `sha256=${crypto
+    .createHmac("sha256", secret)
+    .update(JSON.stringify(req.body))
+    .digest("hex")}`;
+  return req.headers["x-hub-signature-256"] === signature;
+};
 
 app.post("/webhook", async (req, res) => {
   const { repository } = req.body;
   const { username, forkedRepo, accessToken } = req.query;
+  const secret = process.env.secret; // Replace with your actual secret
+
+  // Verify the request signature
+  if (!verifySignature(req, secret)) {
+    return res.status(401).send("Unauthorized");
+  }
 
   if (repository && username && forkedRepo && accessToken) {
     try {
